69ef4c802cb6e73d908ecb79ecb831bdb940c4d0
hasansharifian <hasan_sharifian@yahoo.com>
2026-02-17 20:49:46 +0000

    Deploying to gh-pages from @ hasansharifian/hasansharifian.github.io@b92b2b302b71399eef77402058d06ef57abda6ea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-home",
-          title: "Home",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "Projects",
           description: "",
           section: "Navigation",
